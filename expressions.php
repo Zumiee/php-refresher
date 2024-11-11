d5c86c4f4c6bd2125a1f6fe8caed5f334a82a68c
Jaycee <jccantos1212@gmail.com>
2024-11-11 12:35:31 +0800

    added simple, variable, and arithmetic expression

--- expressions.php
+++ expressions.php
@@ -1,3 +1,17 @@
 <?php 
 
-echo "tanginamo\n";
+echo "Welcome\n";
+
+$name = "Baby";
+echo "Hello, " . $name . "!\n";
+
+
+$mangoPies = 3;
+$slicesPerMangoPie = 8;
+$totalSlices = $mangoPies * $slicesPerMangoPie;
+
+echo "Total mango pie slices: " . $totalSlices . "\n";
+
+$isHungry = false;
+echo $isHungry ? "Yes":"No";
+echo "\n";
