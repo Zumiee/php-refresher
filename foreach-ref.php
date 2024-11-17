6a6bbc72f0933c7fa1aae1da92ba375d914777e4
Jaycee <jccantos1212@gmail.com>
2024-11-18 06:20:22 +0800

    add memory test using reference variable (slower)

---
+++ foreach-ref.php
@@ -0,0 +1,16 @@
+<?php
+
+$largeArray = range(1, 1_000_000);
+$startTime = microtime(true);
+$startMemory = memory_get_usage();
+
+
+foreach($largeArray as &$value) {
+    $value = $value * 2;
+}
+
+$endTime = microtime(true);
+$endMemory = memory_get_usage();
+
+echo "Time " . ($endTime - $startTime) . "seconds\n";
+echo "Memory: " . round(($endMemory - $startMemory) / 1024 / 1024) . "MBs\n";
